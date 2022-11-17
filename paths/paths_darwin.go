33ed516fa148bcb7a265c8d6c57d39867226b61e
Dominik Robert <dmnkrobert@gmail.com>
2022-11-17 21:49:48 +0100

    ci: Update the pipeline with a new go version

---
+++ paths/paths_darwin.go
@@ -0,0 +1,46 @@
+package paths
+
+import (
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+func InitOSSpecific() {
+	viper.SetDefault("configPath", "/etc/monitoring-agent")
+
+	viper.SetDefault("procPath", "/proc")
+	viper.SetDefault("sysPath", "/sys")
+	viper.SetDefault("rootfsPath", "/e")
+	viper.SetDefault("udevDataPath", "/run/udev/data")
+
+	viper.SetDefault("exec.shell", "/bin/bash")
+}
+
+func ProcFilePath(name string) string {
+	return filepath.Join(viper.GetString("procPath"), name)
+}
+
+func SysFilePath(name string) string {
+	return filepath.Join(viper.GetString("sysPath"), name)
+}
+
+func RootfsFilePath(name string) string {
+	return filepath.Join(viper.GetString("rootfsPath"), name)
+}
+
+func UdevDataFilePath(name string) string {
+	return filepath.Join(viper.GetString("rootfsPath"), name)
+}
+
+func RootfsStripPrefix(path string) string {
+	if viper.GetString("udevDataPath") == "/" {
+		return path
+	}
+	stripped := strings.TrimPrefix(path, viper.GetString("rootfsPath"))
+	if stripped == "" {
+		return "/"
+	}
+	return stripped
+}
